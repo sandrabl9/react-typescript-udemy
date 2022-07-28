04fc2b65a089dccc0237e8383f0a2d880f9b2228
Sandra Bretones <sandrabretones@gmail.com>
2022-07-28 12:34:10 +0200

    validaciones formulario tradicional

--- src/03-forms/pages/RegisterPage.tsx
+++ src/03-forms/pages/RegisterPage.tsx
@@ -2,11 +2,13 @@ import { FormEvent } from "react";
 import { useForm } from "../hooks/useForm";
 import "../styless/styles.css";
 
+//Aún sería necesario validar el formulario antes de enviar
 export const RegisterPage = () => {
   const {
     formData,
     onChange,
     resetForm,
+    isValidEmail,
     username,
     email,
     password1,
@@ -29,24 +31,38 @@ export const RegisterPage = () => {
         <input
           type="text"
           placeholder="Name"
-          name="name"
+          name="username"
           value={username}
           onChange={onChange}
+          className={`${username.trim().length <= 0 && "has-error"}`}
         />
+        {username.trim().length <= 0 && (
+          <span>Please enter a valid username</span>
+        )}
         <input
           type="email"
           placeholder="Email"
           name="email"
           value={email}
           onChange={onChange}
+          className={`${!isValidEmail && "has-error"}`}
         />
+        {!isValidEmail(email) && <span>Please enter a valid email</span>}
         <input
           type="password"
           placeholder="Password"
           name="password1"
           value={password1}
           onChange={onChange}
+          className={`${password1.trim().length <= 0 && "has-error"}`}
         />
+        {password1.trim().length <= 0 && (
+          <span>Please enter a valid password</span>
+        )}
+        {password1.trim().length < 6 && password1.trim().length > 0 && (
+          <span>Please enter more than 6 characters</span>
+        )}
+
         <input
           type="password"
           placeholder="Repeat Password"
@@ -54,6 +70,9 @@ export const RegisterPage = () => {
           value={password2}
           onChange={onChange}
         />
+        {password2 !== password1 && (
+          <span>Please enter the same password again</span>
+        )}
 
         <button type="submit">Submit</button>
         <button onClick={resetForm}>Reset</button>
